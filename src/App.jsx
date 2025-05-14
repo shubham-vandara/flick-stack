7ce2628b1fbf79664cfdddab82a3cb26e9f05503
Shubham-Vandara <shubhvandara@gmail.com>
2025-05-14 20:24:52 +0530

    update app icon

--- src/App.jsx
+++ src/App.jsx
@@ -264,7 +264,7 @@ function MovieDetails({
       async function fetchMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
